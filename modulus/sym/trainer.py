3c96cf8523a318db6afe9f3022561d33c35b13fa
Kaustubh Tangsali <71059996+ktangsali@users.noreply.github.com>
2023-10-27 16:34:26 -0700

    Add a fix for gradient aggregation (#82)
    * add a fix for gradient aggregation

--- modulus/sym/trainer.py
+++ modulus/sym/trainer.py
@@ -60,10 +60,18 @@ class AdamMixin:
         self, aggregator: nn.Module, global_optimizer_model: nn.Module, step: int
     ):
         loss, losses = 0, Counter({})
+
+        if self.cfg.cuda_graphs and self.grad_agg_freq != 1:
+            raise ValueError(
+                "Gradient Aggregation with CUDA Graphs is not supported currently."
+            )
+
         for agg_step in range(self.grad_agg_freq):
             with torch.autocast(
                 self.device_amp, enabled=self.amp, dtype=self.amp_dtype
             ):
+                if agg_step != 0:  # load new data for subsequent steps
+                    self.load_data()
                 torch.cuda.nvtx.range_push("Loss computation")
                 losses_minibatch = self.compute_losses(step)
                 torch.cuda.nvtx.range_pop()

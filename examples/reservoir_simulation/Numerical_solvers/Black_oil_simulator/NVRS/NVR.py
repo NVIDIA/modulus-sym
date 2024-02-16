395ee9f1a3a29d5a79369178e266d8960c7dae6d
Clement Etienam <39471613+clementetienam@users.noreply.github.com>
2024-02-16 14:03:05 +0000

    Update NVR.py

--- examples/reservoir_simulation/Numerical_solvers/Black_oil_simulator/NVRS/NVR.py
+++ examples/reservoir_simulation/Numerical_solvers/Black_oil_simulator/NVRS/NVR.py
@@ -1,32 +1,21 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
-//////////////////////////////////////////////////////////////////////////////
-Copyright (C) NVIDIA Corporation.  All rights reserved.
-
-NVIDIA Sample Code
-
-Please refer to the NVIDIA end user license agreement (EULA) associated
-
-with this source code for terms and conditions that govern your use of
-
-this software. Any use, reproduction, disclosure, or distribution of
-
-this software and related documentation outside the terms of the EULA
-
-is strictly prohibited.
-
-//////////////////////////////////////////////////////////////////////////////
-
-Nvidia Finite volume reservoir simulator with flexible solver
-
-AMG to solve the pressure and saturation well possed inverse problem
-
-Geostatistics packages are also provided
-
-@Author: Clement Etienam
-
+@Author : Clement Etienam
 """
-
-#print('.........................IMPORT SOME LIBRARIES.....................')
 
 
 

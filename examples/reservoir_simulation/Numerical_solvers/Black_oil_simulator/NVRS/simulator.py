471edfeb37711b47649487d32a23ff4c1df203b2
Clement Etienam <39471613+clementetienam@users.noreply.github.com>
2024-02-16 14:02:48 +0000

    Update simulator.py

--- examples/reservoir_simulation/Numerical_solvers/Black_oil_simulator/NVRS/simulator.py
+++ examples/reservoir_simulation/Numerical_solvers/Black_oil_simulator/NVRS/simulator.py
@@ -1,23 +1,19 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
-//////////////////////////////////////////////////////////////////////////////
-Copyright (C) NVIDIA Corporation.  All rights reserved.
-
-NVIDIA Sample Code
-
-Please refer to the NVIDIA end user license agreement (EULA) associated
-
-with this source code for terms and conditions that govern your use of
-
-this software. Any use, reproduction, disclosure, or distribution of
-
-this software and related documentation outside the terms of the EULA
-
-is strictly prohibited.
-
-//////////////////////////////////////////////////////////////////////////////
-
-Finite Volume Black Oil Reservoir Simulator
-
 @Author : Clement Etienam
 """
 from __future__ import print_function
